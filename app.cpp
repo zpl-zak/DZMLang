db654dacc2f5b439a9314d01be67a476d919b747
Dominik Madarász <madaraszd@mail.ru>
2016-09-15 22:35:03 +0200

    This one didn't make it, uhh.

--- app.cpp
+++ app.cpp
@@ -3,12 +3,17 @@
 
 #include "src/dzm.hpp"
 
+#include "src/lang/lex/lex_test.hpp"
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int
 main(int argc, char** argv)
 {
+	SET_SYNTAX(test_read);
+	SET_SYNTAX_PAIR(test_read_pair);
+
     test_init(argc, argv);
     
     return(0);
